1543282ce507dcacd759b36966219d972443a64f
Alena_Kr <alenak_2708@mail.ru>
2018-02-06 10:32:55 +0500

    Добавен проект

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//Оператор присваивания =
+var str = "Hello, playground"
+let one = 1
+// Простые операторы
+var summa = 1 + 2 //сложение
+let raznost  = 10 - 4 - 6 // вычитание
+let delenie = 6 / 2 // деление
+let umnozenie = 5 * 3 //умножение
+let trueValue = true // присваивание
+let falseValue = !trueValue // присваивание с не
+summa = 5 - one // вычитание
+let stringSum = "Hi" + "Masha" // сложение строк
+//let stringAndInt = "Hi" + 8 //- ошибка так как операдны должны быть одного типа
+//Опреатор целочисленного деления - возвращает остаток от деления на число
+9 % 4 //остаток от деление = 1
+8 % 4 // остаток от деление = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
